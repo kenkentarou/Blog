e6dfae94f0577c26c98739eb32ea4f437a166370
Taro <zettaigoukaku547@gmail.com>
2020-01-21 23:31:16 +0900

    [add]投稿のテストコード

--- spec/policies/article_policy_spec.rb
+++ spec/policies/article_policy_spec.rb
@@ -1,28 +1,32 @@
 require 'rails_helper'
 
-RSpec.describe ArticlePolicy do
-
-  let(:user) { User.new }
-
-  subject { described_class }
-
-  permissions ".scope" do
-    pending "add some examples to (or delete) #{__FILE__}"
-  end
-
-  permissions :show? do
-    pending "add some examples to (or delete) #{__FILE__}"
-  end
-
-  permissions :create? do
-    pending "add some examples to (or delete) #{__FILE__}"
-  end
-
-  permissions :update? do
-    pending "add some examples to (or delete) #{__FILE__}"
-  end
-
-  permissions :destroy? do
-    pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe '管理画面', type: :system do
+  describe '投稿' do
+    let(:user) { create(:user, name: 'admin' ) }
+    let(:site) { create(:site) }
+    before do
+      user
+      site
+      visit admin_login_identifier_path
+      fill_in 'user_name' ,with: 'admin'
+      click_button '次へ'
+      fill_in 'user_password', with: 'password'
+      click_button 'ログイン'
+    end
+    context '画像を挿入せずに投稿' do
+      it '投稿が成功する' do
+        visit admin_dashboard_path
+        click_link '記事'
+        click_link '新規作成'
+        fill_in 'タイトル', with: Faker::Name.name
+        fill_in 'スラッグ', with: 'abc'
+        click_button '登録する'
+        click_link 'ブロックを追加する'
+        click_link '画像'
+        click_link 'プレビュー'
+        switch_to_window(windows.last)
+        expect(page).to have_selector('.media-image', visible: false)
+      end
+    end
   end
 end
