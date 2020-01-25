a2973b4c9620e7daf7cd208fe9ee700ec98d602b
Taro <zettaigoukaku547@gmail.com>
2020-01-25 12:37:05 +0900

    [modify]テストコード修正（遷移先ページ確認追加、let定義）

--- spec/system/tag_spec.rb
+++ spec/system/tag_spec.rb
@@ -3,20 +3,20 @@ require 'rails_helper'
 RSpec.describe '管理画面', type: :system do
   describe 'タグ' do
     let(:admin) { create(:user) }
+    let(:tag) { create(:taxonomy)}
     context 'パンくず表示' do
       it 'タグ一覧画面でパンくず表示' do
         login_as(admin)
         visit admin_tags_path
         expect(page).to have_css '.breadcrumb'
+        expect(page).to have_link 'Home', href: '/admin/dashboard'
       end
       it 'タグ編集画面でパンくず表示' do
         login_as(admin)
-        visit admin_tags_path
-        fill_in 'タグ名', with: 'あいうえお'
-        fill_in 'スラッグ', with: 'vvvv'
-        click_button '登録する'
-        click_link '編集'
+        tag
+        visit edit_admin_tag_path(tag)
         expect(page).to have_css '.breadcrumb'
+        expect(page).to have_link 'タグ', href: '/admin/tags'
       end
     end
   end
