af9822014a00936edd92a0588b5847d023312f31
Taro <zettaigoukaku547@gmail.com>
2020-01-29 22:01:56 +0900

    [modify]whenever設定ファイル

--- config/schedule.rb
+++ config/schedule.rb
@@ -6,12 +6,15 @@
 # Example:
 #
 # set :output, "/path/to/my/cron_log.log"
+#set :environment, rails_env
+#set :output, "#{Rails.root}/log/cron.log"
 #
-# every 2.hours do
-#   command "/usr/bin/some_great_command"
-#   runner "MyModel.some_method"
-#   rake "some:great:rake:task"
-# end
+require File.expand_path(File.dirname(__FILE__) + "/environment")
+set :output, "#{Rails.root}/log/cron.log"
+
+ every 1.hours do
+   rake "status_task:published"
+ end
 #
 # every 4.days do
 #   runner "AnotherModel.prune_old_records"
