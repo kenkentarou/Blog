e93f1ba8082a41ac67923f8d56c702f192a17358
Taro <zettaigoukaku547@gmail.com>
2020-01-30 12:44:42 +0900

    [modify]cron設定ファイル

--- config/schedule.rb
+++ config/schedule.rb
@@ -10,6 +10,10 @@
 # set :output, "#{Rails.root}/log/cron.log"
 #
 require File.expand_path(File.dirname(__FILE__) + '/environment')
+rails_env = ENV['RAILS_ENV'] || :development
+# cronを実行する環境変数をセット
+set :environment, rails_env
+# cronのログの吐き出し場所
 set :output, "#{Rails.root}/log/cron.log"
 
 every 1.hours do
