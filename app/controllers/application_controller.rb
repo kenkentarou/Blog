43ad3929749998bf1064a13afffa89aabd97c828
Taro <zettaigoukaku547@gmail.com>
2020-02-03 17:28:40 +0900

    [modify]controller側エラーハンドリング

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,14 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  class Forbidden < ActionController::ActionControllerError
+  end
+
+  rescue_from Forbidden, with: :rescue403
+
+  def rescue403
+    render file: 'public/403', status: 403, layout: false
+  end
 
   before_action :require_login
   before_action :current_site
